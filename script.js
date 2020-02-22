87139639883f7d1af0990c2f7756529ea39fc3a2
Def-aki0530 <systemerrrornotfound@gmail.com>
2020-02-22 22:52:11 +0900

    内部リンクへのスクロールアニメーションの追加

--- script.js
+++ script.js
@@ -40,5 +40,12 @@ $(window).scroll(function(){
     }else{
         $('.list-contact a').css('color', '#404040');
     }
+});
 
+$('header a').click(function(){
+    var id = $(this).attr('href');
+    var position = $(id).offset().top;
+    $('html, body').animate({
+      'scrollTop':position
+    },'slow');
 });
